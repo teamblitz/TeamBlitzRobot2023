fae35a729ace20cc0594910f78dbc6715cf8c58d
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2023-03-02 15:54:00 -0700

    arm commands added

---
+++ src/main/java/frc/robot/commands/ArmCommands.java
@@ -0,0 +1,23 @@
+package frc.robot.commands;
+
+public class ArmCommands {
+
+    public void armTop() {}
+
+    public void armMiddle() {}
+
+    public void armBottom() {}
+
+    public void extendedTop() {
+
+    }
+
+    public void extendMiddle() {
+
+    }
+
+    public void extendBottom() {
+        
+    }
+    
+}
