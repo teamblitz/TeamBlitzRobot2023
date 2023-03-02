72e7ed202c39bf3c87548d2699b142c7301deb4f
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2023-03-02 15:45:00 -0700

    arm commands

--- src/main/java/frc/robot/commands/arm/ExtendToCommand.java
+++ src/main/java/frc/robot/commands/arm/ExtendToCommand.java
@@ -1,26 +1,29 @@
 package frc.robot.commands.arm;
 
+import edu.wpi.first.math.estimator.AngleStatistics;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.Arm;
 import frc.robot.subsystems.arm.ArmSubsystem;
 
-public class ExtendToCommand extends CommandBase {
+public class ExtendToCommand extends CommandBase{
 
     private final ArmSubsystem armSubsystem;
     private final double goal;
+    private final double threshold;
 
-    public ExtendToCommand(ArmSubsystem armSubsystem, double goal) {
+    public ExtendToCommand(ArmSubsystem armSubsystem, double goal, double threshold)  {
         this.armSubsystem = armSubsystem;
         this.goal = goal;
+        this.threshold = threshold;
     }
 
     @Override
     public void initialize() {
-        armSubsystem.goTo(null);
+        armSubsystem.extendTo(goal);
     }
 
     @Override
     public boolean isFinished() {
-        return armSubsystem.getState().rotation > goal - 4
-                && armSubsystem.getState().rotation < goal + 4;
+        return armSubsystem.getState().extension > goal - threshold && armSubsystem.getState().extension < goal + threshold;
     }
 }
