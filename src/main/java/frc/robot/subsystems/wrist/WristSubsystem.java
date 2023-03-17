c1c86a71aa4d52e761af7d0bc5e279a871d19674
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2023-03-17 17:51:44 -0600

    wristRotationCommand started

--- src/main/java/frc/robot/subsystems/wrist/WristSubsystem.java
+++ src/main/java/frc/robot/subsystems/wrist/WristSubsystem.java
@@ -1,7 +1,12 @@
 package frc.robot.subsystems.wrist;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.BlitzSubsystem;
+import frc.robot.Constants.Arm;
+import frc.robot.subsystems.arm.ArmSubsystem;
+
 import org.littletonrobotics.junction.Logger;
 
 public class WristSubsystem extends SubsystemBase implements BlitzSubsystem {
@@ -23,5 +28,14 @@ public class WristSubsystem extends SubsystemBase implements BlitzSubsystem {
 
     public void setRotationSpeed(double speed) {
         io.setRotationSpeed(speed);
+    }
+
+    public CommandBase rotateToCommand(double rotation, double threshold, double goal) {
+       /*  return Commands.run(() -> {.armSubsystem
+            armSubsystem.getState().rotation;
+            io.setRotation(1);
+        }, this); */
+        return WristSubsystem.getState().rotation > goal - threshold
+                && WristSubsystem.getState().rotation < goal + threshold;
     }
 }
