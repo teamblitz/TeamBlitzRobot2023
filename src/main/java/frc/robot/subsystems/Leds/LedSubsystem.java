d60a72dc73a115dab2c3f91bd6f5afa0047f4584
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2023-03-16 19:15:13 -0600

    leds

--- src/main/java/frc/robot/subsystems/Leds/LedSubsystem.java
+++ src/main/java/frc/robot/subsystems/Leds/LedSubsystem.java
@@ -2,72 +2,185 @@ package frc.robot.subsystems.Leds;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LedSubsystem extends SubsystemBase {
     private final AddressableLED led;
     private final AddressableLEDBuffer ledBuffer;
 
-    private final long numberOfLEDs = 2;
-    private final double halfWidth = ((double) numberOfLEDs - 1.0) / 2.0;
-    private final int brightness = 255;
-
-
-public LedSubsystem(){
-
     led = new AddressableLED(0);
-    
-    
+
     ledBuffer = new AddressableLEDBuffer(2);
-    led.setLength(ledBuffer.getLength());
 
-    led.start();
-}
-@Override
-public void periodic() {}
+    public LedSubsystem() {
+        led.setLength(ledBuffer.getLength());
 
-public void 
-
-public void setLedSubsystem(double x, double size, int color){
-    long howMany = Math.round(size * 22);
-    long center = Math.round((x + 1) * halfWidth);
-    long start = center - (howMany / 2);
-
-
-int blue;
-int red;
-int green;
-
-if (color == 0) { // If we are purple
-    red = brightness;
-    green = 0;
-    blue = brightness;
+        led.start();
+    }
     
-} else if (color == 1) { // If we are yellow
-    red = brightness;
-    green = brightness;
-    blue = 0;
 
-} else {
-    red = 0;
-    green = 0;
-    blue = 0;
+    public CommandBase coneSlide() {
+        return Commands.runOnce(() -> {
+            ledBuffer.setRGB(0, 255, 255, 0);
+            ledBuffer.setRGB(1, 255, 255, 0);
 
-  for (var i = 0; i < ledBuffer.getLength(); i++) {
-        ledBuffer.setRGB(i, 0, 0, 0);
-}
-for (int i = (int) start; i < start + howMany; i++) {
-    if (i >= 0 && i < numberOfLEDs) {
-        ledBuffer.setRGB(i, red, green, blue);
+            led.setData(ledBuffer);
+        });
     }
-}
-led.setData(ledBuffer);
-}
-}
-public void clear() {
-    for (int i = 0; i < numberOfLEDs; i++) {
-        ledBuffer.setRGB(i, 0, 0, 0);
+
+    public CommandBase  cubeSlide() {
+        return Commands.runOnce(() -> {
+            ledBuffer.setRGB(0, 255, 0, 255);
+            ledBuffer.setRGB(1, 255, 0, 255);
+
+            led.setData(ledBuffer);
+        });
     }
-    led.setData(ledBuffer);
-}
+
+    public CommandBase coneSlideDrop() {
+        return Commands.repeatingSequence(
+            Commands.runOnce(() -> {
+                ledBuffer.setRGB(0, 255, 255, 0);
+                ledBuffer.setRGB(1, 255, 255, 0);
+
+                led.setData(ledBuffer);
+            }, this)
+            .andThen(Commands.waitSeconds(.25))
+            .andThen(Commands.runOnce(() -> {
+                ledBuffer.setRGB(0, 0, 0, 0);
+                ledBuffer.setRGB(1, 0, 0, 0);
+
+                led.setData(ledBuffer);
+            }, this))
+            .andThen(Commands.waitSeconds(.25))
+        );
+    }
+
+    public CommandBase cubeSlideDrop() {
+        return Commands.repeatingSequence(
+            Commands.runOnce(() -> {
+                ledBuffer.setRGB(0, 255, 0, 255);
+                ledBuffer.setRGB(1, 255, 0, 255);
+
+                led.setData(ledBuffer);
+            }, this)
+            .andThen(Commands.waitSeconds(.25))
+            .andThen(Commands.runOnce(() -> {
+                ledBuffer.setRGB(0, 0, 0, 0);
+                ledBuffer.setRGB(1, 0, 0, 0);
+
+                led.setData(ledBuffer);
+            }, this))
+            .andThen(Commands.waitSeconds(.25))
+        );
+    }
+
+    public CommandBase coneLeftShelf() {
+        return Commands.runOnce(() -> {
+            ledBuffer.setRGB(1, 255, 255, 0);
+            ledBuffer.setRGB(1, 0, 0, 0);
+
+            led.setData(ledBuffer);
+        });
+    }
+
+    public CommandBase coneRightShelf (){
+        return Commands.runOnce(() -> {
+            ledBuffer.setRGB(0, 255, 255, 0);
+            ledBuffer.setRGB(1, 0, 0, 0);
+
+            led.setData(ledBuffer);
+        });
+    }
+    
+    public CommandBase cubeLeftShelf() {
+        return Commands.runOnce(() -> {
+            ledBuffer.setRGB(1, 255, 0, 255);
+            ledBuffer.setRGB(1, 0, 0, 0);
+
+            led.setData(ledBuffer);
+        });
+    }
+
+    public CommandBase cubeRightShelf() {
+        return Commands.runOnce(() -> {
+            ledBuffer.setRGB(0, 255, 0, 255);
+            ledBuffer.setRGB(1, 0, 0, 0);
+
+            led.setData(ledBuffer);
+        });
+    }
+
+    public CommandBase coneLeftShielfDrop() {
+        return Commands.repeatingSequence(
+            Commands.runOnce(() -> {
+                ledBuffer.setRGB(1, 255, 0, 255);
+
+                led.setData(ledBuffer);
+            }, this)
+            .andThen(Commands.waitSeconds(.25))
+            .andThen(Commands.runOnce(() -> {
+                ledBuffer.setRGB(1, 0, 0, 0);
+
+                led.setData(ledBuffer);
+            }, this))
+            .andThen(Commands.waitSeconds(.25))
+        );
+    }
+
+    public CommandBase coneRightShielfDrop() {
+        return Commands.repeatingSequence(
+            Commands.runOnce(() -> {
+                ledBuffer.setRGB(0, 255, 0, 255);
+
+                led.setData(ledBuffer);
+            }, this)
+            .andThen(Commands.waitSeconds(.25))
+            .andThen(Commands.runOnce(() -> {
+                ledBuffer.setRGB(0, 0, 0, 0);
+
+                led.setData(ledBuffer);
+            }, this))
+            .andThen(Commands.waitSeconds(.25))
+        );
+    }
+
+    public CommandBase cubeLeftShielfDrop() {
+        return Commands.repeatingSequence(
+            Commands.runOnce(() -> {
+                ledBuffer.setRGB(1, 255, 0, 255);
+
+                led.setData(ledBuffer);
+            }, this)
+            .andThen(Commands.waitSeconds(.25))
+            .andThen(Commands.runOnce(() -> {
+                ledBuffer.setRGB(1, 0, 0, 0);
+
+                led.setData(ledBuffer);
+            }, this))
+            .andThen(Commands.waitSeconds(.25))
+        );
+    }
+
+    public CommandBase cubeRightShielfDrop() {
+        return Commands.repeatingSequence(
+            Commands.runOnce(() -> {
+                ledBuffer.setRGB(0, 255, 0, 255);
+
+                led.setData(ledBuffer);
+            }, this)
+            .andThen(Commands.waitSeconds(.25))
+            .andThen(Commands.runOnce(() -> {
+                ledBuffer.setRGB(0, 0, 0, 0);
+
+                led.setData(ledBuffer);
+            }, this))
+            .andThen(Commands.waitSeconds(.25))
+        );
+    }
+
+    
+
 }
